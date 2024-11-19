400185e5ba766bf4def385419ce0a93f99c0ff99
richardhapb <richard.penab@gmail.com>
2024-11-19 04:03:26 -0300

    feat: deployment and metadata

--- dashboard/index.py
+++ dashboard/index.py
@@ -16,9 +16,10 @@ from train import train
 init_mlflow()
 
 model = None
+last_model = 0
 
 def load_model():
-    global model
+    global model, last_model
     MODEL_NAME="XGBClassifier"
     last_model = ML.get_last_model(MODEL_NAME)
     model = mlflow.sklearn.load_model(f"models:/{MODEL_NAME}/{last_model}")
@@ -44,7 +45,44 @@ names = {
 
 TZ = "America/Santiago"
 
-app = Dash(__name__, update_title=None)
+app = Dash(__name__, update_title=None, meta_tags=[
+        {
+            "name": "viewport",
+            "content": "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+        },
+        {
+            "name": "description",
+            "content": (
+                "Dashboard interactivo para el análisis del tráfico vehicular en Antofagasta. "
+                "Predicciones basadas en machine learning con datos de la API de Waze."
+            ),
+        },
+        {
+            "name": "keywords",
+            "content": (
+                "tráfico vehicular, Antofagasta, análisis de tráfico, Dash, machine learning, "
+                "predicción de tráfico, rutas seguras, API Waze, eventos de tráfico, accidentes"
+            ),
+        },
+        {
+            "name": "author",
+            "content": "Richard Peña B.",
+        },
+        {
+            "property": "og:title",
+            "content": "Gestión del Tráfico Vehicular en Antofagasta - Dashboard",
+        },
+        {
+            "property": "og:description",
+            "content": (
+                "Explora patrones de tráfico y predicciones de rutas seguras con un dashboard"
+                "basado en datos de la API de Waze y técnicas de machine learning."
+            ),
+        },
+    ],)
+app._favicon = ("favicon.png")
+app.title = "Gestión del tráfico en Antofagasta"
+server = app.server
 
 since = int(
     datetime.datetime(
@@ -445,6 +483,7 @@ app.layout = html.Div(
                                                 "border": "1px solid #555",
                                             },
                                         ),
+                                        html.P(f"Versión del modelo: {last_model}", id="model_version")
                                     ],
                                     className="plot-container",
                                 ),
