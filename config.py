420181ad4c4d80e4d672138d6475300d7fc38e0a
richardhapb <richard.hapb@icloud.com>
2025-03-31 12:27:27 -0300

    feat: remove database config

--- config.py
+++ config.py
@@ -5,13 +5,5 @@ load_dotenv()
 
 WAZE_API_URL = os.getenv("WAZE_API_URL")
 
-DATABASE_CONFIG = {
-    "host": os.getenv("DB_HOST", "localhost"),
-    "port": int(os.getenv("DB_PORT", "5432")),
-    "user": os.getenv("DB_USER", "postgres"),
-    "password": os.getenv("DB_PASSWORD", ""),
-    "database": os.getenv("DB_DATABASE", "waze"),
-}
-
 SERVER_URL = os.getenv("SERVER_URL")
 
